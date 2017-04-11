32abd8f43532490f7279eadfd839cd7bc534220f
Dinesh V <dineshshabde@gmail.com>
2017-04-11 09:29:41 +0530

    Add Main class where execution starts

---
+++ March Lunchtime 2017/Task 2 - Bear and Species/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
+        // Implement solution
+    }
+}
